1484e404174a5531c45d07f67c44d2609cef6f2c
aplaksin <aplaksin@ecocompany.ru>
2020-03-04 14:24:50 +0300

    init

---
+++ example/src/main/java/com/javatechie/spring/angular/example/dto/Book.java
@@ -0,0 +1,14 @@
+package com.javatechie.spring.angular.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+    private int id;
+    private String name;
+    private int price;
+}
